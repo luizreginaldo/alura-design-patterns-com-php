6e75a3d4257fb757b59e10ea09a6a6342f428472
Luiz Reginaldo Silva <contato@luizreginaldo.com>
2022-02-01 08:54:41 -0300

    Command Handlers

--- gera-pedido.php
+++ gera-pedido.php
@@ -3,10 +3,12 @@
 require_once 'vendor/autoload.php';
 
 use Alura\DesignPattern\GerarPedido;
+use Alura\DesignPattern\GerarPedidoHandler;
 
 $valorOrcamento = $argv[1];
 $numeroDeItens = $argv[2];
 $nomeCliente = $argv[3];
 
 $gerarPedido = new GerarPedido($valorOrcamento, $numeroDeItens, $nomeCliente);
-$gerarPedido->execute();
+$gerarPedidoHandler = new GerarPedidoHandler();
+$gerarPedidoHandler->execute($gerarPedido);
---
+++ src/GerarPedidoHandler.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Alura\DesignPattern;
+
+class GerarPedidoHandler
+{
+    public function __construct(/* PedidoRepository, MailService */)
+    {
+    }
+
+    public function execute(GerarPedido $gerarPedido)
+    {
+        $orcamento = new Orcamento();
+        $orcamento->quantidadeItens = $gerarPedido->getNumeroItens();
+        $orcamento->valor = $gerarPedido->getValorOrcamento();
+
+        $pedido = new Pedido();
+        $pedido->dataFinalizacao = new \DateTimeImmutable();
+        $pedido->nomeCliente = $gerarPedido->getNomeCliente();
+        $pedido->orcamento = $orcamento;
+
+        //PedidosRepository
+        echo "Cria pedido no banco de dados " . PHP_EOL;
+
+        //MailService
+        echo "Envia e-mail para cliente " . PHP_EOL;
+
+        //LogService
+        echo "Gerar log de criação de pedido " . PHP_EOL;
+    }
+}
